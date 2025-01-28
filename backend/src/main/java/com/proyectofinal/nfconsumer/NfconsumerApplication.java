41e405ceeee5bb2e51a922568e6faede1b3d308b
KhardiCr <dev.cramos@gmail.com>
2025-01-28 19:18:17 +0100

    Skeleton Project
    First push with empty spring project, and basic dependencies

---
+++ backend/src/main/java/com/proyectofinal/nfconsumer/NfconsumerApplication.java
@@ -0,0 +1,13 @@
+package com.proyectofinal.nfconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfconsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfconsumerApplication.class, args);
+	}
+
+}
